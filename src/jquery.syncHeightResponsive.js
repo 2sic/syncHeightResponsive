8aa8d6b8751124581879be654fe9adbabe2b25b3
Raphael Müller <raaphii@gmail.com>
2016-11-15 15:04:17 +0100

    Some changes to simplify usage: - now always uses the same css selector .sync-height-with-neighbors - all elements having this css class are selected and processed while syncing height - subsequent calls to $.syncHeightResponsive will update the element list

--- src/jquery.syncHeightResponsive.js
+++ src/jquery.syncHeightResponsive.js
@@ -1,5 +1,6 @@
+'use strict';
 /**
-    2sic sync-height jQuery plugin
+    2sic sync-height
     https://github.com/raphael-m/syncHeightResponsive/
     Syncs the height of elements that are displayed beneath each other (by comparing their offset top)
     Include also the waitForImages plugin to ensure correct behaviour when elements contain images
@@ -7,82 +8,86 @@
 
 (function ($) {
 	// Prevent duplicate initialization
-    if($.fn.syncHeightResponsive)
+    if($.syncHeightResponsive)
 		return;
-	
-    $.fn.syncHeightResponsive = function (config) {
+    
+    // Elements array will contain all elements which have been added to be synced
+    var selector = '.sync-height-with-neighbors';
+    var elementsToSync = [];
+
+    // The main function to be called from outside - triggers the height sync
+	$.syncHeightResponsive = function() {
+        elementsToSync = $(selector);
+    }
+
+    // The main function - run height sync
+    var triggerHeightSync = function () {
         
-        // Self will contain the elements to sync
-        var self = $(this);
+        // Group elements by getting the offset top value
+        var previousTop = 0,
+            elementGroups = {};
 
-        // The main function - run height sync
-        var triggerHeightSync = function () {
+        $(elementsToSync).each(function (i, e) {
 
-            // Group elements by getting the offset top value
-            var previousTop = 0,
-            	elementGroups = {};
+            var top = Math.round($(e).offset().top);
 
-            $(self).each(function (i, e) {
+            if (!elementGroups[top])
+                elementGroups[top] = [];
+            elementGroups[top].push(e);
 
-                var top = Math.round($(e).offset().top);
-
-                if (!elementGroups[top])
-                    elementGroups[top] = [];
-                elementGroups[top].push(e);
-
-            });
-
-            // All groups are created, sync height for each group separately
-            $.each(elementGroups, function (k, elements) {
-
-                // Get maximal height value 
-                var maxHeight = 0;
-                $(elements).css('height', '');
-
-                $.each(elements, function (i, element) {
-                    maxHeight = Math.max(maxHeight, $(element).height());
-                });
-
-                $(elements).height(maxHeight);
-
-            });
-
-        }
-
-        // Call sync wrapped in a timeout to make sure other scripts that could modify the height have already run
-        window.setTimeout(function () {
-            triggerHeightSync();
-        }, 0);
-
-        $(window).load(function () {
-            triggerHeightSync();
         });
 
-        if ($.fn.waitForImages) {
-        	$(self).waitForImages(triggerHeightSync);
-        }
+        // All groups are created, sync height for each group separately
+        $.each(elementGroups, function (k, elements) {
 
-	    var ticking = false;
-	    var update = function() {
-	    	ticking = false;
-		    triggerHeightSync();
-	    };
+            // Get maximal height value 
+            var maxHeight = 0;
+            var $elements = $(elements);
+            $elements.css('height', '');
 
-        $(window).resize(function () {
-        	if (!ticking)
-        		window.requestAnimationFrame(update);
-        	ticking = true;
+            $.each(elements, function (i, element) {
+                maxHeight = Math.max(maxHeight, $(element).height());
+            });
+
+            $elements.height(maxHeight);
+
         });
 
+    }
+
+    // Call sync wrapped in a timeout to make sure other scripts that could modify the height have already run
+    window.setTimeout(function () {
+        triggerHeightSync();
+    }, 0);
+
+    $(window).load(function () {
+        triggerHeightSync();
+    });
+
+    if ($.fn.waitForImages) {
+        $(elementsToSync).waitForImages(triggerHeightSync);
+    }
+
+    var ticking = false;
+    var update = function() {
+        ticking = false;
+        triggerHeightSync();
     };
+
+    $(window).resize(function () {
+        if (!ticking)
+            window.requestAnimationFrame(update);
+        ticking = true;
+    });
+
 })(jQuery);
 
 /* Polyfill for requestAnimationFrame
 	Source: http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
 */
 (function() {
-		if(window.requestAnimationFrame)
-			return;
+    if(window.requestAnimationFrame)
+        return;
 			
     var lastTime = 0;
     var vendors = ['webkit', 'moz'];
